3c703c8e7efeaf37c8f3a07884945f639e6b931a
David Draguta <daviddraguta@gmail.com>
2020-02-09 13:53:27 -0500

    New bar.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,6 +3,8 @@
 killall -q polybar
 
 #wait until polybar is shut down.
-while pgrep -u $UID -x polybar > /dev/null; do sleep .1; done
+while pgrep -u $UID -x polybar > /dev/null; do sleep .05; done
 
-polybar -c ~/.config/polybar/config main &
+polybar -c ~/.config/polybar/main.ini main &
+polybar -c ~/.config/polybar/side_bar1.ini side_bar1 &
+polybar -c ~/.config/polybar/side_bar2.ini side_bar2 &
---
+++ polybar/scripts/update.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+NOTIFY_ICON=/usr/share/icons/Papirus/32x32/apps/system-software-update.svg
+
+get_total_updates() {
+    UPDATES=$(checkupdates 2>/dev/null | wc -l);
+    MESSAGE="THERE ARE $UPDATES UPDATES AWAITING YOU, SIRE!.";
+}
+
+while true; do
+    get_total_updates
+
+    if hash notify-send &>/dev/null; then
+        if (( UPDATES > 50 )); then
+            notify-send -u critical -i $NOTIFY_ICON $MESSAGE
+        elif (( UPDATES > 25 )); then
+            notify-send -u normal -i $NOTIFY_ICON $MESSAGE
+        elif (( UPDATES > 2 )); then
+            notify-send -u low -i $NOTIFY_ICON $MESSAGE
+        fi
+    fi
+
+    while (( UPDATES > 0 )); do
+        if (( UPDATES == 1 )); then
+            echo "$UPDATES Update"
+        elif (( UPDATES > 1)); then
+            echo "$UPDATES Updates"
+        else
+            echo $BAR_ICON 
+        fi
+        sleep 10
+        get_total_updates
+    done
+
+
+    while (( UPDATES == 0 )); do
+        echo $BAR_ICON
+        sleep 1800
+        get_total_updates
+    done
+done
