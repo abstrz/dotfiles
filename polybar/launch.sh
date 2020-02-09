35766322b97b7e6f4794c4a6d80d7c1ff143e825
David Draguta <daviddraguta@gmail.com>
2020-02-09 11:54:25 -0500

    New polybar.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env sh
 
-## Add this to your wm startup file.
-
-# Terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+#wait until polybar is shut down.
+while pgrep -u $UID -x polybar > /dev/null; do sleep .1; done
 
-polybar -c ~/.config/polybar/config_top.ini main & 
-polybar -c ~/.config/polybar/config_top.ini side1 & 
-polybar -c ~/.config/polybar/config_top.ini side2 &
-polybar -c ~/.config/polybar/config_bottom.ini main
-
+polybar -c ~/.config/polybar/config main &
