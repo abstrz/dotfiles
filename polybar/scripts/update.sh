c3446bdc45cec8a5cf7dbdc83e4aabedc102d2a9
David Draguta <daviddraguta@gmail.com>
2020-02-12 10:27:08 -0500

    Simplify .zshrc and tweak updates module.

--- polybar/scripts/update.sh
+++ polybar/scripts/update.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
+
+BAR_ICON=""
 NOTIFY_ICON=/usr/share/icons/Papirus/32x32/apps/system-software-update.svg
 
 get_total_updates() {
     UPDATES=$(checkupdates 2>/dev/null | wc -l);
-    MESSAGE="THERE ARE $UPDATES UPDATES AWAITING YOU, SIRE!.";
 }
 
 while true; do
     get_total_updates
-
     if hash notify-send &>/dev/null; then
         if (( UPDATES > 50 )); then
-            notify-send -u critical -i $NOTIFY_ICON $MESSAGE
+            notify-send -u critical -i $NOTIFY_ICON "THERE ARE $UPDATES UPDATES AWAITING YOU, SIRE."
         elif (( UPDATES > 25 )); then
-            notify-send -u normal -i $NOTIFY_ICON $MESSAGE
+            notify-send -u normal -i $NOTIFY_ICON "THERE ARE $UPDATES UPDATES AWAITING YOU, SIRE."
         elif (( UPDATES > 2 )); then
-            notify-send -u low -i $NOTIFY_ICON $MESSAGE
+            notify-send -u low -i $NOTIFY_ICON "THERE ARE $UPDATES UPDATES AWAITING YOU."
         fi
     fi
 
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,3 +1,6 @@
+source ~/.profile
+
+
 export EDITOR=vim
 export TERMINAL="st"
 export BROWSER=firefox
@@ -66,8 +69,7 @@ PERL_MB_OPT="--install_base \"/home/david/perl5\""; export PERL_MB_OPT;
 PERL_MM_OPT="INSTALL_BASE=/home/david/perl5"; export PERL_MM_OPT;
 
 #scripts
-source ~/tools/scripts/defs.sh
-source ~/.profile
+source ~/scripts/defs.sh
 
 export beats_id=DC:D3:A2:C4:41:1F
 export _JAVA_AWT_WM_NONREPARENTING=1
