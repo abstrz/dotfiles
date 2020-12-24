860389216cb386e07b21571ea16235e803ef70ed
David Draguta <daviddraguta@gmail.com>
2020-12-24 13:59:06 -0500

    -m

--- main.sh
+++ main.sh
@@ -2,15 +2,21 @@ handle(){
   for folder in $PWD/src/*; do
     source $folder/init.sh &&
       for file in ${files[@]}; do 
-        if [ "$1" == "-sf" ] || [ ! -e "$target_path/$file" ]; then 
-          if [ ! -d "$target_path" ]; then
-            echo "Creating $dir..." &&
-            mkdir $dir > /dev/null 
+        echo "Do you want to link $file (y/n, n is default)?"
+        read skip
+        if [ "$skip" == "y" ]; then 
+          if [ "$1" == "-sf" ] || [ ! -e "$target_path/$file" ]; then 
+            if [ ! -d "$target_path" ]; then
+              echo "Creating $dir..." &&
+              mkdir $dir > /dev/null 
+            fi
+            echo "Symbolically linking $file..."  &&
+            doas ln $1 $folder/$file $target_path > /dev/null
+          else 
+            echo "$target_path/$file exists. Skipping..."
           fi
-          echo "Symbolically linking $file..."  &&
-          doas ln $1 $folder/$file $target_path > /dev/null
         else 
-          echo "$target_path/$file exists. Skipping..."
+          echo "Skipping..."
         fi
       done
     done
