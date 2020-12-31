187e3e9ecd297de04234bbdf97d68936f91f5409
David Draguta <daviddraguta@gmail.com>
2020-12-31 08:41:23 -0500

    Rewrite it in zsh, so that it can be sourced in .zshrc.

--- main.sh
+++ main.sh
@@ -1,20 +1,21 @@
 handle(){
-  for folder in $PWD/src/*; do
+  for folder in $HOME/Scripts/dotfiles/src/*; do
     source $folder/data &&
-      shopt -s dotglob     &&
+      setopt globdots   &&
       for file_path in $folder/*; do 
         file=`basename $file_path`
-        if [ "$file" != "data" ]; then 
+        if [[ "$file" != "data" ]]; then 
           display_name="`basename $folder`/`basename $file_path`"
-          if [ "$1" == "-sf" ] || [ ! -e "$target_path/$file" ]; then 
+
+          if [[ "$1" == "-sf" ]] || [[ ! -e "$target_path/$file" ]]; then 
             echo "Do you want to link $display_name y/n (n is default, q to quit)?"
             read option
-            if [ "$option" == "q" ]; then 
+            if [[ "$option" == "q" ]]; then 
               echo "Quitting..."
               return -1
             fi
-            if [ "$option" == "y" ]; then 
-              if [ ! -d "$target_path" ]; then
+            if [[ "$option" == "y" ]]; then 
+              if [[ ! -d "$target_path" ]]; then
                 echo "Creating $target_path..." &&
                   mkdir $target_path > /dev/null 
               fi
@@ -35,15 +36,14 @@ dotfiles(){
     return -1 
   fi 
   if [[ $input == y || $input == yes || $input == Y || $input == Yes ]]; then 
-    handle "-sf"
+    handle "-sf";
     return 1
   elif [[ $input == n || $input == no || $input == N || $input == No ]]; then 
-    handle "-s"
+    handle "-s";
     return 1
   else
     echo "Sorry, unrecognized option" 
     return 1
   fi
 }
-
 
